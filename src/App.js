d81789d9476bc1388d42fea2a43b908f66b6148c
123pratu <pratikshakorde3006@gmail>
2023-05-20 22:10:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname:"",
+    lname:"",
+    email:"",
+    phone:"",
+  });
+
+
+  const inputEvent= (event)=>{
+    const {value, name} = event.target;
+
+
+    setFullName((preValue)=>{ 
+       return {
+        ...preValue,
+        [name]: value,
+        
+       }
+    });
+  }
+
+  const  onSubmits=(event)=>{
+    event.preventDefault();
+    alert("form Submited");
+  }
+
+  return(
+    <>
+    <div className='main_div'>  
+    <form onSubmit={onSubmits}>
+    <div>
+      <h1>
+      Hello {fullName.fname} {fullName.lname}
+      </h1>
+       <p>{fullName.email}</p>
+       <p>{fullName.phone}</p>
+
+      <input type="text" 
+       placeholder="Enter Your Name"
+      name='fname'
+      onChange = {inputEvent}  
+       value={fullName.fname}
+       />
+       <br/>
+       <input type="text" 
+      placeholder="Enter Your lastName"
+        name='lname'
+         onChange = {inputEvent}  
+       value={fullName.lname}
+       />
+       <br/>
+       <input type="email" 
+      placeholder='Enter Your Email'
+        name='email'
+         onChange = {inputEvent}  
+         value={fullName.email}
+       />
+       <br/>
+       <input type="number" 
+       placeholder='Enter Your Phone number.'
+        name='phone'
+         onChange = {inputEvent}  
+       value={fullName.phone}
+       />
+       <br/>
+      <button type="submit">Submit Me 👍</button>
     </div>
+    </form>
+    </div>
+    </>
   );
 }
+
 
 export default App;
